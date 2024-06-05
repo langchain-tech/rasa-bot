3b4d9139dc36f5109a7c7ee00667c9d08f5dc70d
prince-bluebash <prince@bluebash.co>
2024-06-05 10:54:05 +0530

    add programs

--- actions/actions.py
+++ actions/actions.py
@@ -1,10 +1,131 @@
 
+import os
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 import pandas as pd
 import pdb
+import requests
+
+
+
+class DataloadAPI(object):
+
+    def __init__(self):
+        self.db = pd.read_csv("/home/bluebash-005/code/bluebash/rasa chatbot/zappy/actions/weight_management_plans.csv")
+
+    def fetch_data(self):
+        return self.db.head()
+
+    def format_data(self, df, header=True) -> Text:
+        return df.to_csv(index=False, header=header)
+
+
+class ChatGPT(object):
+
+    def __init__(self):
+        self.url = "https://api.openai.com/v1/chat/completions"
+        self.model = "gpt-3.5-turbo"
+        self.headers={
+            "Content-Type": "application/json",
+            "Authorization": f"Bearer {os.getenv('OPENAI_API_KEY')}"
+        }
+        self.prompt = "Answer the following question, based on the data shown. " \
+            "Answer in a complete sentence and don't say anything else."
+
+    def ask(self, restaurants, question):
+        content  = self.prompt + "\n\n" + restaurants + "\n\n" + question
+        body = {
+            "model":self.model, 
+            "messages":[{"role": "user", "content": content}]
+        }
+        result = requests.post(
+            url=self.url,
+            headers=self.headers,
+            json=body,
+        )
+        return result.json()["choices"][0]["message"]["content"]
+
+
+
+dataload_api = DataloadAPI()
+chatGPT = ChatGPT()
+
+class ActionShowPrograms(Action):
+
+    def name(self) -> Text:
+        return "action_show_program"
+
+    def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        data = dataload_api.fetch_data()
+        results = dataload_api.format_data(data)
+        #readable = dataload_api.format_data(data[['Plan Duration']], header=False)
+        readable = data[['Plan Duration']].to_string(index=True)
+        dispatcher.utter_message(text=f"Here are some programs:\n\n{readable} \n\n please select a number like 0,1,2 to know more about that program...")
+
+        return [SlotSet("results", results)]
+
+
+
+
+# class ActionShowDetail(Action):
+#     def name(self) -> Text:
+#         return "action_show_details"
+
+#     def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+#         previous_results = tracker.get_slot("results")
+#         program_number = tracker.latest_message["text"]
+#         print(program_number)
+
+#         if int(program_number)>=0 and int(program_number)<3:
+#             req=f"what is the benifts of {program_number} program number \n here 0 programs number means (6 Months Plan) \n 1 program number means (3 Months Plan)  2 program number means Trial Plan 21 Days ."
+#             print(req)
+#             answer = chatGPT.ask(previous_results, req)
+#             dispatcher.utter_message(text = answer)
+#         else:
+#             req="Data is not availbe with this program number please recheck it and try again.."
+#             dispatcher.utter_message(text = req)
+
+#         return []
+
+
+
+class ActionShowDetail(Action):
+    def name(self) -> Text:
+        return "action_show_details"
+
+    def run(self, dispatcher: CollectingDispatcher,tracker: Tracker,domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        program_number = tracker.latest_message["text"]
+        print(program_number)
+
+        program_number=int(program_number)
+        if program_number>=0 and program_number<3:
+            df = pd.read_csv("/home/bluebash-005/code/bluebash/rasa chatbot/zappy/actions/weight_management_plans.csv")
+            if program_number==0:
+                result = df[df['Plan Duration'] == "6 Months Plan"]
+            elif program_number==1:
+                result = df[df['Plan Duration'] == "3 Months Plan"]
+            else:
+                result = df[df['Plan Duration'] == "Trial Plan 21 Days"]
+            result_dict = result.to_dict(orient='records')
+            text_data = ""
+            for item in result_dict:
+                for key, value in item.items():
+                    text_data += f"{key}: {value}\n"
+
+            print(text_data)
+            dispatcher.utter_message(text = text_data)
+        else:
+            result="Data is not available with this program number please recheck it and try again.."
+            dispatcher.utter_message(text = result)
+        
+
+        return []
+
 
 class ActionOrderDetails(Action):
 
